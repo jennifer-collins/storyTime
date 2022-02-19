896154de2ac6719402d38c76370c5b5c43c37138
jennifer-collins <jennifercollins_@hotmail.com>
2022-02-19 15:22:54 -0500

    Added keyboard accessibility, updated book subjects on selection, edited the app instructions, updates to hover and focus states

--- script.js
+++ script.js
@@ -1,83 +1,48 @@
-// User navigates to landing page with drop down menu containing pre-selected book subjects
-
-
-// API DATA
-// create url to include the specific search parameters of pre-selected subject types (ex. canadian_poetry, romance, mystery etc.)
-// use fetch('url') syntax to return the data in json format to console
-// store the object data in variables
-// object data we would like to appear on page:
-// Book Title
-// Author
-// Book Cover Photo (in small, medium or large)
-
-// once data is stored in these variables, create a section on the page and display the suggestions on the page
-// append the information to the page using innerHTML or appendChild
-// user will see 10-12 book suggestions returned on page based on the drop down menu selections
-
-// FORM
-// Add values of pre-selected book subjects to form options
-// use document.querySelector to select element on form we would like to append these options to
-// using separate template expressions to place the different search values into the dropdown list
-
-// SEARCH FEATURE (STRETCH GOAL)
-// create a search bar and submit button with event listener to remove default behaviour on 'submit'
-// store values of this search in a variable that is converted to remove spaces " " and replace with "_" (as per syntax in API documentation)
-// return this search and 10-12 specific book recommendations on page
-// data to appear on page:
-// Book Title
-// Author
-// Book Cover
-
-// FILTER (stretch goal)
-// filter data by language 
-
-
-// BOOK DESCRIPTION (STRETCH GOAL)
-// Have the book description appear on click
-// add an event listener to the ul, then target the div within the li's to have the book description appear on 'click'
-// target the element we want to create, create variable to store description data, append data to the page
-// create a div within the li's with a class of bookDescription
-// hide the div with display: none; until the event listener of 'click' is activated
-
 // Namespace for App
 const bookApp = {};
 
 const form = document.querySelector('form');
 
+// Event Listener to get book subject
 bookApp.getSubject = function () {
     form.addEventListener('submit', function (event) {
         event.preventDefault();
         bookApp.getBooks();
-
-        const mainSection = document.querySelector('main');
-        mainSection.classList.add('newBackgroundColor');
     })
 }
 
 
-// getting data from api
+// Getting data from API
 bookApp.getBooks = function () {
-    // create our url endpoint
-
+    // create our url endpoint based on users selection
     const selection = document.querySelector('#bookSubject');
     let selectedValue = selection.value
     const apiKey = 'AIzaSyBt9dJvG6Epw3nwxoLVa_AU_4CzUGOdWzc'
+    
     // Note: Didn't use the URl constructor below because it wouldn't allow us to leave the q parameter blank and search for subject
     const bookUrl = `https://www.googleapis.com/books/v1/volumes?q=+subject:${selectedValue}&orderBy=newest&maxResults=12&langRestrict=en&key=${apiKey}`;
 
+    // If statement to prevent submitting form without selection
+    if (selection.value === 'initial'){
+        alert('Please select a subject!')
+    } 
+    else {
+        const mainSection = document.querySelector('main');
+        mainSection.classList.add('newBackgroundColor');
 
+        fetch(bookUrl)
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (jsonResponse) {
+                const worksData = jsonResponse.items;
+                bookApp.displayBooks(worksData);
+            })
+    }
 
-    fetch(bookUrl)
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (jsonResponse) {
-            const worksData = jsonResponse.items;
-            bookApp.displayBooks(worksData);
-        })
 }
 
-// adding book data to the page
+// Adding book data to the page
 bookApp.displayBooks = function (dataFromApi) {
 
     const bookUl = document.querySelector('.bookResults');
@@ -91,14 +56,13 @@ bookApp.displayBooks = function (dataFromApi) {
     bookResultsHeading.textContent = "here are your book suggestions:";
     bookResultsHeadingContainer.appendChild(bookResultsHeading);
 
+    // Use loop to create an li element for each result in the array that displays: cover, title, author, description and learn more link
     dataFromApi.forEach(function (bookObject) {
         // List of Books display and text creation --------
         const bookList = document.createElement('li');
         bookList.classList.add('bookListLi');
         const title = document.createElement('h3');
         const author = document.createElement('h4');
-
-        // Add image element when we get book cover data from API
         const image = document.createElement('img');
 
         // Book Description display and text creation
@@ -108,18 +72,23 @@ bookApp.displayBooks = function (dataFromApi) {
         descriptionContainer.classList.add('bookDescriptionContainer');
         googleButton.classList.add('googleBookLink');
 
+        // Info from API
         title.textContent = bookObject.volumeInfo.title;
         author.textContent = bookObject.volumeInfo.authors;
         description.textContent = bookObject.volumeInfo.description;
         googleButton.textContent = "Learn More"
 
-
+        // Creating image and info links
         image.src = bookObject.volumeInfo.imageLinks.thumbnail;
         image.alt = `${bookObject.volumeInfo.title} cover`
         googleButton.href = bookObject.volumeInfo.infoLink
         googleButton.target = "_blank"
         googleButton.rel = "noopener noreferrer";
 
+        // Tab index for accessibility
+        bookList.tabIndex = "0";
+
+        // Appending all data to the page
         bookList.appendChild(image);
         bookList.appendChild(title);
         bookList.appendChild(author);
@@ -128,12 +97,24 @@ bookApp.displayBooks = function (dataFromApi) {
         descriptionContainer.appendChild(description);
         bookUl.appendChild(bookList);
 
+        // Event listener that allows li to hide the image and show the description
         bookList.addEventListener('click', function () {
             bookList.classList.toggle('fullWidth')
             image.classList.toggle('hidden');
             author.classList.toggle('hidden');
             description.classList.toggle('show');
             googleButton.classList.toggle('show');
+        })
+
+        // Added accessibility fix for focus/keyboard use
+        bookList.addEventListener('keypress', function(event) {
+            if (event.key === 'Enter') {
+                bookList.classList.toggle('fullWidth')
+                image.classList.toggle('hidden');
+                author.classList.toggle('hidden');
+                description.classList.toggle('show');
+                googleButton.classList.toggle('show');
+            }
         })
     })
 }
